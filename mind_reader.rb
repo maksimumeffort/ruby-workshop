85041cbe4ac3b322e0a2f64efa6efee3a22f1f89
Alex M <alexmaksimets@gmail.com>
2021-08-27 20:41:54 +1000

    clean up code

--- mind_reader.rb
+++ mind_reader.rb
@@ -22,24 +22,6 @@
 #   puts "that's right"
 # end
 
-# def mind_reader
-#   magic_number = 7
-#   max_guesses = 3
-#   attempts = 0
-#   guess = nil
-#   while guess != magic_number do
-#     print "Nope! Try again! " unless guess.nil?
-#     puts "What's your guess?"
-#     guess = gets.chomp.to_i
-#     break if attempts >= max_guesses
-#       attempts += 1
-#     end
-#   winner = "You've guessed it!"
-#   loser = "You ran out of guesses, try again later!"
-#   puts guess == magic_number ? winner : loser
-# end
-
-
 
 def mind_reader
   magic_number = 7
