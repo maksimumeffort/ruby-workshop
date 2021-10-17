63b288192e8dccc4aadfd733d7579d8c85b18295
Alex M <alexmaksimets@gmail.com>
2021-10-17 19:58:14 +1000

    combined both functions into one method. fully working

--- number_guessing.rb
+++ number_guessing.rb
@@ -1,27 +1,33 @@
-def play_choice
-  puts "welcome to HiLow - Shall we play? [Y/n]"
+def play_game
+  # initital greeting
+  puts 'welcome to HiLow - Shall we play? [Y/n]'
   choice = gets.chomp.upcase
-  if choice == "Y"
+
+  # acceptance logic logic
+  if choice == 'Y'
     puts "I'm going to pick a random number you have to guess. Enter maximum number for guessing range."
     max_num = gets.chomp.to_i
-  else
-    puts "Thanks for playing!"
-  end
-end
 
-def guess(max_num)
-  random_number = rand(max_num)
-  input = nil
-  puts "guess the number"
-  while input != random_number
-    input = gets.chomp.to_i
-    if random_number > input
-      puts "go high"
-    else
-      puts "go low"
+    # high/low functionality
+    random_number = rand(max_num)
+    input = nil
+    puts 'guess the number'
+
+    # guessing loop
+    while input != random_number
+      input = gets.chomp.to_i
+      if random_number > input
+        puts 'go high'
+      else
+        puts 'go low'
+      end
     end
+    puts 'you guessed correctly'
+
+  # refusal logic
+  else
+    puts 'Thanks for playing!'
   end
-    puts "you guessed correctly"
 end
 
-guess(5)
+play_game
