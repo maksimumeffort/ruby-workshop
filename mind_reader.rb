4158ffd583f0c704fc9b168e655af52567c02474
Alex M <alexmaksimets@gmail.com>
2021-10-17 16:42:40 +1000

    fixed the max_guesses bug

--- mind_reader.rb
+++ mind_reader.rb
@@ -1,13 +1,19 @@
 def mind_reader(range)
   magic_number = rand(range)
+  max_guesses = 3
+  attempts = 0
   guess = nil
 
   while guess != magic_number do
     puts "try again" unless guess.nil?
     puts "guess the number"
+    attempts += 1
     guess = gets.chomp.to_i
+    break if attempts >= max_guesses
+
   end
-    puts "magic number is #{magic_number}"
+
+    puts guess == magic_number ? "Correct" : "magic number is #{magic_number}"
 end
 
 mind_reader(20)
