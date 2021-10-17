77a1f53474a3386d2181af4eb33503097d533976
Alex M <alexmaksimets@gmail.com>
2021-10-17 19:19:34 +1000

    yielding 3 variables

--- math_method.rb
+++ math_method.rb
@@ -1,16 +1,26 @@
+# def math(array)
+#   first_item = array.shift
+
+#   while(array.length > 0) do
+#     second_item = array.shift
+#     first_item = yield first_item, second_item
+#   end
+#     return first_item
+# end
+
+
+# math([4, 5, 6]) { |a, b| a + b }
+# math([4, 5, 6]) { |a, b| a * b }
+# math([4, 5, 6]) { |a, b| a - b }
+# math([4, 5, 6]) { |a, b| a / b }
+# math([4, 5, 6]) { |a, b| a / b.to_f }
+
 def math(array)
   first_item = array.shift
-
-  while(array.length > 0) do
-    second_item = array.shift
-    first_item = yield first_item, second_item
+  while(array.length > 1) do
+    second_item = array.shift || 0
+    third_item = array.shift || 0
+    first_item = yield first_item, second_item, third_item
   end
     return first_item
 end
-
-
-math([4, 5, 6]) { |a, b| a + b }
-math([4, 5, 6]) { |a, b| a * b }
-math([4, 5, 6]) { |a, b| a - b }
-math([4, 5, 6]) { |a, b| a / b }
-math([4, 5, 6]) { |a, b| a / b.to_f }
