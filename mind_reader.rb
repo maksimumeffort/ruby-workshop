9348e915e5331c4c0c0ac5ee0fc5b0fdba6a595e
Alex M <alexmaksimets@gmail.com>
2021-10-17 16:28:11 +1000

    mind_reader loop working

--- mind_reader.rb
+++ mind_reader.rb
@@ -1,46 +1,12 @@
-# def mind_reder(range)
-#   magic_number = rand(range)
-#   puts "what's your guess?"
-#   guess = gets.chomp.to_i
-#   if guess == magic_number
-#     puts "correct"
-#   else
-#     puts "incorrect.magic number is #{magic_number}"
-#   end
-# end
-
-# mind_reder(1..10)
-
-# def mind_reader
-#   magic_number = 7
-#   guess = nil
-#   while guess != magic_number do
-#     puts "try again" unless guess.nil?
-#     puts "what's your guess?"
-#     guess = gets.chomp.to_i
-#   end
-#   puts "that's right"
-# end
-
-
-def mind_reader
-  magic_number = 7
-  max_guesses = 3
-  attempts = 0
-  guess = nil
+def mind_reader(range)
+  magic_number = rand(range)
+  guess = 0
 
   while guess != magic_number do
-    puts "Nope, try again!" unless guess.nil?
-    puts "what's your guess?"
+    puts "guess the number"
     guess = gets.chomp.to_i
-    break if attempts >= max_guesses
-      attempts += 1
-    end
-
-  winner = 'good guess'
-  loser = 'you ran out of guesses'
-  puts guess == magic_number ? winner : loser
+  end
+    puts "magic number is #{magic_number}"
 end
 
-mind_reader
-
+mind_reader(20)
