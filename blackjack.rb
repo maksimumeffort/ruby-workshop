ae0ff456fb58b7203c490eb0cb44914f664205d7
Alex M <alexmaksimets@gmail.com>
2021-10-19 21:39:03 +1000

    wrote pseudocode

---
+++ blackjack.rb
@@ -0,0 +1,23 @@
+# single player, computer dealer
+# objective: get cards closest to 21
+# if over 21: lose
+# player_score > dealer_score && player_score <= 21 ? win : lose
+# player can only see 1 dealer card
+# player gets a card
+# card = rand(1...13)
+# 4 suits: diamonds, clubs, hearts, spades
+# Player interface. 2 options: Hit => add 1 card, stay => keep player_score
+
+# cards_deck method
+
+# shuffle_cards method
+
+# identify_card method
+
+# label_card method
+
+# card_value method
+
+# determine_winner method
+
+# game_loop method
