6b138c0e1487c71e3f91114240faf44ecc6cd19b
Alex M <alexmaksimets@gmail.com>
2021-10-20 07:55:37 +1000

    interface greeting working

--- blackjack.rb
+++ blackjack.rb
@@ -1,10 +1,28 @@
+# interface
+
+  # greeting
+  puts 'Enter your name'
+  player_name = gets.chomp
+
+  puts "Welcome #{player_name}"
+
 # single player, computer dealer
+# player_score = nil
+# dealer_score = nil
+
 # objective: get cards closest to 21
+
+# card = rand(1...13)
+
+# def draw_card
+#   player_score += card
+# end
+
 # if over 21: lose
 # player_score > dealer_score && player_score <= 21 ? win : lose
 # player can only see 1 dealer card
 # player gets a card
-# card = rand(1...13)
+
 # 4 suits: diamonds, clubs, hearts, spades
 # Player interface. 2 options: Hit => add 1 card, stay => keep player_score
 
