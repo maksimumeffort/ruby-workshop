3b5bc31ce3f76bbd0b55996921a55b4a8cf85f2d
Alex M <alexmaksimets@gmail.com>
2021-11-08 11:34:29 +1000

    separated deck generation into generate_deck and shuffle_deck methods

--- blackjack.rb
+++ blackjack.rb
@@ -1,16 +1,8 @@
 # mid level logic, score comparison with cards
 
   # methods
-    def score_count(player, dealer)
-      puts "Dealer has #{dealer.sum}"
-      puts player.sum > dealer.sum ? 'You win!' : 'You lose'
-    end
 
-    def player_choice(cards)
-      puts "You have #{cards.sum}"
-      puts 'Your move. [hit/stay]'
-    end
-
+    # generate_deck method
     def generate_deck
       suits = %w[D C H S]
       values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
@@ -21,53 +13,76 @@
       end
     end
 
+    # shuffle_deck method
+    def shuffle_deck
+      generate_deck.flatten(1).shuffle
+    end
+
     def draw_card(side, cards)
       side << cards.pop
     end
 
+    # identify_card method
+    # label_card method
+    # card_value method
     def card_score(side)
       score = 0
       side.each do |s|
         # needs to first check if s[0] is A, if so it can be either 11 or 1
-        # need 2 types of counters then:
-        # 1 counts if A is 11, other counts if 1
-        # if score is less than threshold Ace equals 11
-
-        s[0].class == Integer ? score += s[0] : score += 10
+        if s[0] == 'A' && score < 21
+          score += 11
+        elsif s[0] == 'A' && score > 21
+          score += 1
+        # needs an if, elsif, else
+        else
+          s[0].class == Integer ? score += s[0] : score += 10
+        end
       end
       score
     end
 
   # first cards
-  dealer_cards = []
-  player_cards = []
-  deck = generate_deck.flatten(1).shuffle
+  # dealer_cards = []
+  # player_cards = []
+  # dealer_score = card_score(dealer_cards)
+  # player_score = card_score(player_cards)
+
+
+  deck = shuffle_deck
+  #generate_deck.flatten(1).shuffle
+
+  print deck
 
   # interface
-    # choice = nil
+  #   choice = nil
 
-    # while choice != 'n'
-    #   puts 'Do you want to play a hand? Y/n'
-    #   choice = gets.chomp.downcase
-    # end
+  #   # while choice != 'n'
+  #   #   puts 'Do you want to play a hand? Y/n'
+  #   #   choice = gets.chomp.downcase
+  #   # end
 
   # 2.times { draw_card(dealer_cards, deck) }
-  # draw_card(player_cards)
+  # draw_card(player_cards, deck)
 
-  # puts "Dealer has #{dealer_cards[0]}"
+  # # puts "Dealer has #{dealer_cards[0]}"
 
-  # player choice logic
-  # while player_cards.sum <= 21
-  #   player_choice(player_cards)
+  # # player choice logic
+  # # game_loop method
+  # while player_score <= 21
+  #   # player_choice(player_cards)
+  #   puts 'your choice. [hit/stay]'
   #   choice = gets.chomp.downcase
 
   #   case choice
-  #   when 'hit' then draw_card(player_cards)
+  #   when 'hit' then
+  #     draw_card(player_cards, deck);
+  #     card_score(player_cards)
   #   when 'stay' then break
   #   else puts 'Not an option'
   #   end
   # end
 
+  # determine_winner method
   # puts player_cards.sum > 21 ? "You're over 21. You lose" : score_count(player_cards, dealer_cards)
 
 # single player, computer dealer
@@ -84,16 +99,15 @@
 # 4 suits: diamonds, clubs, hearts, spades
 # Player interface. 2 options: Hit => add 1 card, stay => keep player_score
 
-# cards_deck method
 
-# shuffle_cards method
+#Dealer hitting rules
+  # the dealer's hand is not complete until all players have either received blackjack, busted, or stayed.
+  # When the cards are dealt at the beginning of the round the dealer will draw 2 cards for themselves and one card will be hidden.
+  # After all, players go, the dealer will then reveal the hidden card and will hit until they are at or higher than 17. At which point the dealer will stay.
+  # Some tables will or online sites will have the dealer stop in Blackjack at 16, so make sure you read the table's rules
 
-# identify_card method
 
-# label_card method
 
-# card_value method
 
-# determine_winner method
 
-# game_loop method
+
